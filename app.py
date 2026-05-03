1d6c542e35212c6d4a5934e66235ef06a19bbe93
amanpy45-png <263108657+amanpy45-png@users.noreply.github.com>
2026-05-03 19:21:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.write("Compare Bi-LSTM vs Transformer")
 # =========================
 @st.cache_resource
 def load_lstm_model():
-     url = "https://drive.google.com/uc?id=1fuc8GRgjm95J-bqBi_F6jq6aKmzJeqlF"
+    url = "https://drive.google.com/uc?id=1fuc8GRgjm95J-bqBi_F6jq6aKmzJeqlF"
     output = "lstm_model.keras"
     if not os.path.exists(output):
         gdown.download(url, output, quiet=False)
